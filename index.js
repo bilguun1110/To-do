a98a758102e4a5b4b59d2d783fac50889bd2d6d7
binderiyabilguun@gmail.com <23LP0562@G302-CC0145.local>
2023-12-11 11:22:22 +0800

    dec11

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ const desc_input = document.getElementById("desc_input");
 const select_status = document.getElementById("select_status");
 const select_priority = document.getElementById("select_priority");
 const add_button = document.getElementById("add_button");
+const cards = document.querySelectorAll(".card");
+const todo_cont = document.getElementById("to_do-container");
+const progress_container = document.getElementById("progress_container");
+const stuck_container = document.getElementById("stuck_container");
+const done_container = document.getElementById("done_container");
 
 let state = [];
 const firstGet = JSON.parse(localStorage.getItem("result-1"));
 state = firstGet ? firstGet : [];
-
-console.log(state);
 
 let result = { title: "", desc: "", status: "Todo", priority: "low" };
 
@@ -54,16 +57,13 @@ add_button.addEventListener("click", () => {
   select_status.value = "Todo";
   select_priority.value = "low";
   myModal.style.display = "none";
-  number_todo.innerHTML = task_todo.length;
-  number_progress.innerHTML = task_progress.length;
-  number_stuck.innerHTML = task_stuck.length;
-  number_done.innerHTML = task_done.length;
 });
 
 const cardComponent = (props) => {
   const { title, desc, status, priority } = props;
+  const uniqId = "id" + Math.random().toString(16).slice(2);
 
-  return `<div class=${status} draggable="true">
+  return `<div class="${status} results" id=${uniqId} draggable="true">
   <div class="main_content">
   <div class="check">
     <i class="fa-regular fa-circle-check"></i>
@@ -81,11 +81,17 @@ const cardComponent = (props) => {
     `;
 };
 
+let task_todo = document.getElementsByClassName("Todo");
+let task_progress = document.getElementsByClassName("Inprogress");
+let task_stuck = document.getElementsByClassName("Stuck");
+let task_done = document.getElementsByClassName("Done");
+
+let number_todo = document.getElementById("number_todo");
+let number_progress = document.getElementById("number_progress");
+let number_stuck = document.getElementById("number_stuck");
+let number_done = document.getElementById("number_done");
+
 const render = () => {
-  const todo_cont = document.getElementById("to_do-container");
-  const progress_container = document.getElementById("progress_container");
-  const stuck_container = document.getElementById("stuck_container");
-  const done_container = document.getElementById("done_container");
   const response = JSON.parse(localStorage.getItem("result-1"));
   todo_cont.innerHTML = "";
   progress_container.innerHTML = "";
@@ -109,40 +115,54 @@ const render = () => {
         break;
     }
   });
+  number_todo.innerHTML = task_todo.length;
+  number_progress.innerHTML = task_progress.length;
+  number_stuck.innerHTML = task_stuck.length;
+  number_done.innerHTML = task_done.length;
 };
 render();
 
-let task_todo = document.getElementsByClassName("Todo");
-let task_progress = document.getElementsByClassName("Inprogress");
-let task_stuck = document.getElementsByClassName("Stuck");
-let task_done = document.getElementsByClassName("Done");
+// drag and drop
+const allTasks = document.querySelectorAll(".results");
 
-let number_todo = document.getElementById("number_todo");
-let number_progress = document.getElementById("number_progress");
-let number_stuck = document.getElementById("number_stuck");
-let number_done = document.getElementById("number_done");
+let temp;
+const todoCard = document.getElementById("to-do");
+const progressCard = document.getElementById("progress");
+const stuckCard = document.getElementById("stuck");
+const doneCard = document.getElementById("done");
 
-let task = document.getElementById("component");
-
-for (el of task_todo) {
-  task_todo.addEventListener("dragstart", (event) => {
-    let selected = event.target.id;
-    event.dataTransfer.setData("myId", selected);
+Array.prototype.forEach.call(allTasks, (el) => {
+  el.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("todo", event.target.id);
   });
-}
-for (el of task) {
-  task.addEventListener("dragover", (event) => {
+});
+cards.forEach((el) => {
+  el.addEventListener("dragover", (event) => {
     event.preventDefault();
   });
-  task.addEventListener("drop", (event) => {
-    const divId = event.dataTransfer.getData("myId");
-    const div = document.getElementById(divId);
-    task.appendChild(div);
-  });
-}
-for (el of task_progress) {
-  task_progress.addEventListener("dragstart", (event) => {
-    let selected = event.target.id;
-    event.dataTransfer.setData("myId", selected);
-  });
-}
+});
+
+todoCard.addEventListener("drop", (event) => {
+  temp = event.dataTransfer.getData("todo");
+  const draggedTodo = document.getElementById(temp);
+
+  todo_cont.appendChild(draggedTodo);
+});
+
+progressCard.addEventListener("drop", (event) => {
+  temp = event.dataTransfer.getData("todo");
+  const draggedProg = document.getElementById(temp);
+  progress_container.appendChild(draggedProg);
+});
+stuckCard.addEventListener("drop", (event) => {
+  temp = event.dataTransfer.getData("todo");
+  const draggedStuck = document.getElementById(temp);
+  stuck_container.appendChild(draggedStuck);
+});
+doneCard.addEventListener("drop", (event) => {
+  temp = event.dataTransfer.getData("todo");
+  const draggedDone = document.getElementById(temp);
+  done_container.appendChild(draggedDone);
+});
+
+// status oorchloh
